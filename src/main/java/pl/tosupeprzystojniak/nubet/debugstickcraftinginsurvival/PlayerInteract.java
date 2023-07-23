239aa48d68a6c141bc6e23bfedd6a0a9884fd09a
TheLittle_Yang <2570984321@qq.com>
2023-07-24 02:05:39 +0800

    update

--- src/main/java/pl/tosupeprzystojniak/nubet/debugstickcraftinginsurvival/PlayerInteract.java
+++ src/main/java/pl/tosupeprzystojniak/nubet/debugstickcraftinginsurvival/PlayerInteract.java
@@ -1,5 +1,6 @@
 package pl.tosupeprzystojniak.nubet.debugstickcraftinginsurvival;
 
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -20,13 +21,19 @@ public class PlayerInteract implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
+        GameMode playerGameMode = player.getGameMode();
         Action action = event.getAction();
         List<String> excluded_blocks = plugin.getConfig().getStringList("excluded_blocks");
 
         if (player.getInventory().getItemInMainHand().getType().equals(Material.DEBUG_STICK)) {
+            if (!player.hasPermission("minecraft.debugstick")) {
+                player.sendMessage("抱歉，只有建筑师可以使用调试棒。");
+                return;
+            }
+
             Block clickedblock = event.getClickedBlock();
             if (clickedblock != null && action == Action.RIGHT_CLICK_BLOCK) {
-                if (excluded_blocks.contains(clickedblock.getType().toString())) {
+                if (!playerGameMode.equals(GameMode.CREATIVE) && excluded_blocks.contains(clickedblock.getType().toString())) {
                     event.setCancelled(true);
                     player.sendMessage("你不能对这种类型的方块使用调试棒！");
                 }
